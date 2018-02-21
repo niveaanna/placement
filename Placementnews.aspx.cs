9a1cdb6cfe588c45059b968560e1a503c15fc654
niveaanna <nivea13annageorge@gmail.com>
2018-02-21 16:20:03 +0530

    Add files via upload

---
+++ Placementnews.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace placement
+{
+    public partial class Placementnews : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=placement;Integrated Security=True";
+            con.Open();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String ins = "insert into Placementnews values ('" + txtcompanyname.Text + "','" + txtpost.Text + "','" + txtcriteria.Text + "','" + txtyear.Text + "','" + txtvenue.Text + "','" + txtinformation.Text +"')";
+
+            SqlCommand cmd = new SqlCommand(ins, con);
+
+
+            cmd.ExecuteNonQuery();
+            Response.Write("saved sucessfully");
+
+            txtcompanyname.Text = "";
+            txtpost.Text = "";
+            txtcriteria.Text = "";
+            txtyear.Text = "";
+            txtvenue.Text = "";
+            txtinformation.Text = "";
+            
+       
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            txtcompanyname.Text = "";
+            txtpost.Text = "";
+            txtcriteria.Text = "";
+            txtyear.Text = "";
+            txtvenue.Text = "";
+            txtinformation.Text = "";
+        }
+    }
+}
---
+++ category.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace placement
+{
+    public partial class category : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=placement;Integrated Security=True";
+            con.Open();
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+         //   String ins = "insert into category values ('" + DropDownList1.SelectedItem.Text.Trim() + "',)";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String ins = "insert into category (Category_name) values('" + DropDownList1.SelectedItem.Text.Trim() + "')";
+            SqlCommand cmd = new SqlCommand(ins, con);
+            cmd.ExecuteNonQuery();
+            //DropDownList1.Text = "";
+            Response.Write("<script LANGUAGE='JavaScript'>alert('Data Added Successfullyy')</script>");
+        }
+    }
+}
