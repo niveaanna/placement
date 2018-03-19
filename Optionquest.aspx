c28f5a2681e6ac2136bf03f9cfa7b4796d9507c0
niveaanna <nivea13annageorge@gmail.com>
2018-03-19 12:00:38 +0530

    Add files via upload

---
+++ Optionquest.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/student.Master" AutoEventWireup="true" CodeBehind="Optionquest.aspx.cs" Inherits="Placement_Cell.Optionquest" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Repeater ID="Repeater1" runat="server" OnItemCommand="Repeater1_ItemCommand">
+        <ItemTemplate>
+                <p>
+                <asp:Label ID="Label" runat="server" Text='<%#Eval("Question") %>'></asp:Label>
+                    </p>
+            </ItemTemplate>
+    </asp:Repeater>
+</asp:Content>
+
---
+++ Optionquest.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Placement_Cell
+{
+    public partial class Optionquest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                getData(this.User.Identity.Name);
+
+            }
+        }
+        private void getData(string user)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=placement;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select distinct Question from tbl_question", con);
+            cmd.ExecuteNonQuery();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                Repeater1.DataSource = dt;
+                Repeater1.DataBind();
+
+            }
+        }
+
+        protected void Repeater1_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+
+        }
+    }
+}
