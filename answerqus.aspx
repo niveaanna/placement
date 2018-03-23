c60bb55426941c27c31475ed75d06c2ef62634ac
niveaanna <nivea13annageorge@gmail.com>
2018-03-23 10:33:44 +0530

    Add files via upload

---
+++ answerqus.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/student.Master" AutoEventWireup="true" CodeBehind="answerqus.aspx.cs" Inherits="Placement_Cell.answerqus" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    
+<!DOCTYPE html>
+
+
+    <div>
+        
+            
+       <asp:Repeater ID="rptQues" runat="server"  OnItemDataBound="Repeater1_ItemDataBound">
+       
+            
+        <ItemTemplate>
+       
+          <asp:HiddenField ID="HiddenField1" runat="server" Value='<%# Eval("Qus_id") %>' Visible="False" />
+               
+               
+                <asp:Label ID="lblques" runat="server" Text='<%# Eval("Question") %>' />
+            <asp:RadioButtonList ID="rdoTest" runat="server"></asp:RadioButtonList>
+         <%--   <asp:PlaceHolder ID="plchldr" runat="server">
+
+
+            </asp:PlaceHolder>--%>
+            <%--<asp:RadioButtonList runat="server" ID="rdolst"></asp:RadioButtonList>--%> 
+                   <asp:Repeater ID="rptOption" runat="server">                        
+                        <ItemTemplate >
+                            
+                            <%-- <asp:RadioButton ID="rbopt" runat="server" Text='<%# Eval("Options") %>' />--%>
+                           <%-- <asp:RadioButton  GroupName="abc" runat="server" Text='<%# Eval("Options") %>' />--%>
+                            <asp:RadioButtonList ID="RadioButtonList1" runat="server" DataValueField='<%# Eval("Options") %>'>
+                               <%-- <asp:ListItem Text='<%# Eval("Options") %>'></asp:ListItem>--%>
+                            </asp:RadioButtonList>
+
+                        </ItemTemplate>
+                        
+                    </asp:Repeater>
+            <br />
+             
+    </ItemTemplate>
+   </asp:Repeater>
+        
+            
+    </div>
+    <asp:Button ID="Button1" runat="server" Text="SUBMIT" />
+
+</asp:Content>
---
+++ answerqus.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Placement_Cell
+{
+    public partial class answerqus : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                getData(this.User.Identity.Name);
+            }
+
+        }
+
+        private void getData(string user)
+        {
+
+            SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=placement;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select distinct Question,Qus_id from tbl_question", con);
+            cmd.ExecuteNonQuery();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+          
+            if (dt.Rows.Count > 0)
+            {
+                rptQues.DataSource = dt;
+                rptQues.DataBind();
+
+            }
+        }
+
+        protected void Repeater1_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+
+                SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=placement;Integrated Security=True");
+                con.Open();
+                //HiddenField hdn = new HiddenField();hdn= (HiddenField)FindControl("HiddenField1");
+                // string carrierName = ((String)e.Item.DataItem)["carrierName"].ToString();
+                DataRowView dr = (DataRowView)e.Item.DataItem;
+                string strcarrierName = Convert.ToString(dr["Qus_id"]);
+                SqlCommand cmd = new SqlCommand("select Options from Options where Qus_id= '" + strcarrierName + "'", con);
+                cmd.ExecuteNonQuery();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    
+                    
+                    Repeater rptOrders = e.Item.FindControl("rptOption") as Repeater;
+                    rptOrders.DataSource = dt;
+                    rptOrders.DataBind();
+                    //RadioButtonList rdoLst = new RadioButtonList();
+                    //rdoLst.DataSource = dt;
+                    //rdoLst.DataTextField = "Options";
+                    //rdoLst.DataBind();
+
+                    RadioButtonList rdot = e.Item.FindControl("rdoTest") as RadioButtonList;
+                    rdot.DataSource = dt;
+                    rdot.DataTextField = "Options";
+                    rdot.DataBind();
+
+
+                    //PlaceHolder plc = e.Item.FindControl("plchldr") as PlaceHolder;
+                    //plc.Controls.Add(rdoLst);
+
+                }
+            }
+
+
+        }
+    }
+}
+        
